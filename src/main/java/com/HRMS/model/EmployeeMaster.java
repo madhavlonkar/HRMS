8e2488c74f17d5f0231bf5d9e114e374ec2992a2
GURU <gururajyadav.sae.comp@gmail.com>
2023-04-14 11:39:41 +0530

    updates in demand and employee masters

--- src/main/java/com/HRMS/model/EmployeeMaster.java
+++ src/main/java/com/HRMS/model/EmployeeMaster.java
@@ -1,61 +1,108 @@
 package com.HRMS.model;
 
+import java.sql.Blob;
 import java.sql.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="Employee_Master")
 public class EmployeeMaster {
 	
 	@Id
-	@Column(name="empid")
-	private int empid;
+	@Column(name="EmpId")
+	private Integer empid;
 	
-	@Column(name="empname")
+	@Column(name="EmpName")
 	private String empname;
 	
-	@Column(name="empaddress")
+	@Column(name="EmpAddress")
 	private String empaddress;
 	
-	@Column(name="empstate")
+	@Column(name="EmpState")
 	private String empstate;
 	
-	@Column(name="empemail")
+	@Column(name="EmpEmail")
 	private String empemail;
 	
-	@Column(name="empmobileno")
+	@Column(name="EmpMobileNo")
 	private String empmobileno;
 	
-	@Column(name="empgender")
+	@Column(name="EmpGender")
 	private String empgender;
+
+	@Column(name="EmpDob")
+	private Date empDob;
 	
-	@Column(name="emppan")
+	@Column(name="EmpPan")
 	private String emppan;
+
+	@Column(name="EmpAdhar")
+	private String empAdhar;
 	
-	@Column(name="empDOJ")
+	@Column(name="EmpDoj")
 	private Date empDOJ;
 	
-	@Column(name="empdepartment")
+	@Column(name="EmpDepartment")
 	private String empdepartment;
 	
-	@Column(name="empdesignation")
+	@Column(name="EmpDesignation")
 	private String empdesignation;
 	
-	@Column(name="empbankaccno")
+	@Column(name="EmpBankAccNo")
 	private String empbankaccno;
 	
-	@Column(name="empbankname")
+	@Column(name="EmpBankName")
 	private String empbankname;
 
-	public int getEmpid() {
+	@Column(name = "EmpResume")
+	private Blob empResume;
+
+	@Column(name = "EmpStatus")
+	private String empStatus;
+
+	@Column(name = "ManagerId")
+	private Integer ManagerId;
+
+	@OneToOne
+	@JoinColumn(name = "ProjectID")
+	private ProjectMaster Project_Id;
+
+	@OneToOne
+	@JoinColumn(name = "DemandID")
+	private DemandMaster demand_id;
+
+	public EmployeeMaster() {
+	}
+
+	public EmployeeMaster(Integer empid, String empname, String empaddress, String empstate, String empemail, String empmobileno, String empgender, Date empDob, String emppan, String empAdhar, Date empDOJ, String empdepartment, String empdesignation, String empbankaccno, String empbankname, Blob empResume, String empStatus, Integer managerId, ProjectMaster project_Id, DemandMaster demand_id) {
+		this.empid = empid;
+		this.empname = empname;
+		this.empaddress = empaddress;
+		this.empstate = empstate;
+		this.empemail = empemail;
+		this.empmobileno = empmobileno;
+		this.empgender = empgender;
+		this.empDob = empDob;
+		this.emppan = emppan;
+		this.empAdhar = empAdhar;
+		this.empDOJ = empDOJ;
+		this.empdepartment = empdepartment;
+		this.empdesignation = empdesignation;
+		this.empbankaccno = empbankaccno;
+		this.empbankname = empbankname;
+		this.empResume = empResume;
+		this.empStatus = empStatus;
+		ManagerId = managerId;
+		Project_Id = project_Id;
+		this.demand_id = demand_id;
+	}
+
+	public Integer getEmpid() {
 		return empid;
 	}
 
-	public void setEmpid(int empid) {
+	public void setEmpid(Integer empid) {
 		this.empid = empid;
 	}
 
@@ -107,12 +154,28 @@ public class EmployeeMaster {
 		this.empgender = empgender;
 	}
 
+	public Date getEmpDob() {
+		return empDob;
+	}
+
+	public void setEmpDob(Date empDob) {
+		this.empDob = empDob;
+	}
+
 	public String getEmppan() {
 		return emppan;
 	}
 
 	public void setEmppan(String emppan) {
 		this.emppan = emppan;
+	}
+
+	public String getEmpAdhar() {
+		return empAdhar;
+	}
+
+	public void setEmpAdhar(String empAdhar) {
+		this.empAdhar = empAdhar;
 	}
 
 	public Date getEmpDOJ() {
@@ -155,37 +218,69 @@ public class EmployeeMaster {
 		this.empbankname = empbankname;
 	}
 
-	public EmployeeMaster(int empid, String empname, String empaddress, String empstate, String empemail,
-			String empmobileno, String empgender, String emppan, Date empDOJ, String empdepartment,
-			String empdesignation, String empbankaccno, String empbankname) {
-		super();
-		this.empid = empid;
-		this.empname = empname;
-		this.empaddress = empaddress;
-		this.empstate = empstate;
-		this.empemail = empemail;
-		this.empmobileno = empmobileno;
-		this.empgender = empgender;
-		this.emppan = emppan;
-		this.empDOJ = empDOJ;
-		this.empdepartment = empdepartment;
-		this.empdesignation = empdesignation;
-		this.empbankaccno = empbankaccno;
-		this.empbankname = empbankname;
+	public Blob getEmpResume() {
+		return empResume;
 	}
 
-	public EmployeeMaster() {
-		super();
-		// TODO Auto-generated constructor stub
+	public void setEmpResume(Blob empResume) {
+		this.empResume = empResume;
+	}
+
+	public String getEmpStatus() {
+		return empStatus;
+	}
+
+	public void setEmpStatus(String empStatus) {
+		this.empStatus = empStatus;
+	}
+
+	public Integer getManagerId() {
+		return ManagerId;
+	}
+
+	public void setManagerId(Integer managerId) {
+		ManagerId = managerId;
+	}
+
+	public ProjectMaster getProject_Id() {
+		return Project_Id;
+	}
+
+	public void setProject_Id(ProjectMaster project_Id) {
+		Project_Id = project_Id;
+	}
+
+	public DemandMaster getDemand_id() {
+		return demand_id;
+	}
+
+	public void setDemand_id(DemandMaster demand_id) {
+		this.demand_id = demand_id;
 	}
 
 	@Override
 	public String toString() {
-		return "EmployeeMaster [empid=" + empid + ", empname=" + empname + ", empaddress=" + empaddress + ", empstate="
-				+ empstate + ", empemail=" + empemail + ", empmobileno=" + empmobileno + ", empgender=" + empgender
-				+ ", emppan=" + emppan + ", empDOJ=" + empDOJ + ", empdepartment=" + empdepartment + ", empdesignation="
-				+ empdesignation + ", empbankaccno=" + empbankaccno + ", empbankname=" + empbankname + "]";
+		return "EmployeeMaster{" +
+				"empid=" + empid +
+				", empname='" + empname + '\'' +
+				", empaddress='" + empaddress + '\'' +
+				", empstate='" + empstate + '\'' +
+				", empemail='" + empemail + '\'' +
+				", empmobileno='" + empmobileno + '\'' +
+				", empgender='" + empgender + '\'' +
+				", empDob=" + empDob +
+				", emppan='" + emppan + '\'' +
+				", empAdhar='" + empAdhar + '\'' +
+				", empDOJ=" + empDOJ +
+				", empdepartment='" + empdepartment + '\'' +
+				", empdesignation='" + empdesignation + '\'' +
+				", empbankaccno='" + empbankaccno + '\'' +
+				", empbankname='" + empbankname + '\'' +
+				", empResume=" + empResume +
+				", empStatus='" + empStatus + '\'' +
+				", ManagerId=" + ManagerId +
+				", Project_Id=" + Project_Id +
+				", demand_id=" + demand_id +
+				'}';
 	}
-
-
 }
